c30ac90260df549f44168702b746171383273866
Matt Bain <mattbain@slac.stanford.edu>
2025-01-06 18:18:47 -0800

    cleaned up test.py

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
-
-#import ui_MotionStage
-#import ui_Spectrometer
 import ui_StartWindow
 from PyQt6.QtWidgets import QApplication, QWidget
 
 app = QApplication([])
 
-#window = ui_MotionStage.ui_MotionStage()
-#window = ui_Spectrometer.ui_Spectrometer()
 window = ui_StartWindow.ui_StartWindow()
 
 window.show()
